4480685e30bcb854c4f3b9f0700a22953427281d
Shajee98 <shajeebinwaqar@gmail.com>
2021-05-01 07:14:28 +0500

    Ninth Commit

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {createStackNavigator} from '@react-navigation/stack'
 import LoginScreen from './Screens/LoginScreen';
 import RegisterScreen from './Screens/RegisterScreen';
 import HomeScreen from './Screens/HomeScreen';
+import AddChatScreen from './Screens/AddChatScreen';
 
 const Stack = createStackNavigator();
 const globalScreenOptions = {
@@ -23,6 +24,8 @@ export default function App() {
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="AddChat" component={AddChatScreen} />
+      <Stack.Screen name="Chat" component={ChatScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
